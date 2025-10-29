b44cd0d7ec1ba196d82e3b297a079c3bcf1da0ba
BenjoLiving <ben.livingstone670@gmail.com>
2025-10-29 13:51:27 -0600

    bambi model comparisons

--- matching_pennies/analysis/experiments/lesion_study_normal.py
+++ matching_pennies/analysis/experiments/lesion_study_normal.py
@@ -3,6 +3,7 @@ import polars as pl
 import pandas as pd 
 import bambi as bmb
 import arviz as az 
+import pymc as pm
 from matching_pennies.io.metrics_store import load_metrics
 
 EXPERIMENT = "lesion_study"
@@ -22,20 +23,17 @@ efs_model_cols = [
     "EFS_before_flg",
     "treatment",
     "trial_idx",
-    "session_idx",
     "animal_id",
 ]
 
 efs_ses1 = tdf_ses1[:, efs_model_cols]
+efs_ses1 = efs_ses1.filter(pl.col("EFS_before_flg").is_not_null())
 efs_ses1.drop_nulls()
 
 # Scale both columns in place
 efs_ses1 = efs_ses1.with_columns([
     ((pl.col("trial_idx") - pl.col("trial_idx").min()) / 
-    (pl.col("trial_idx").max() - pl.col("trial_idx").min())).alias("trial_idx_scaled"),
-
-    ((pl.col("session_idx") - pl.col("session_idx").min()) / 
-    (pl.col("session_idx").max() - pl.col("session_idx").min())).alias("session_idx_scaled")
+    (pl.col("trial_idx").max() - pl.col("trial_idx").min())).alias("trial_idx_scaled")
 ])
 
 efs_ses1 = efs_ses1.to_pandas()
@@ -45,13 +43,34 @@ efs_ses1["treatment"] = pd.Categorical(efs_ses1["treatment"],
                                 categories=["sham", "ofc", "mpfc"],
                                 ordered=True)
 
-# Define model structure 
-efs_model = "EFS_before_flg ~ treatment*trial_idx_scaled + treatment*session_idx_scaled + (1|animal_id)"
+# Define model structures
+efs_model_intercept = "EFS_before_flg ~ treatment*trial_idx_scaled + (1|animal_id)"
+efs_model_slope = "EFS_before_flg ~ treatment*trial_idx_scaled + (1 + trial_idx_scaled|animal_id)"
 
 # Define model and fit it
-model = bmb.Model(
-    efs_model,
+model_intercept = bmb.Model(
+    efs_model_intercept,
     efs_ses1,
     family="bernoulli",  # logistic link by default
 )
-idata = model.fit()
+model_slope = bmb.Model(
+    efs_model_slope,
+    efs_ses1, 
+    family="bernoulli"
+)
+
+idata_intercept = model_intercept.fit(chains=4, cores=1, log_likelihood=True)
+idata_slope = model_slope.fit(chains=4, cores=1, log_likelihood=True)
+
+print("===========================================")
+print("           RANDOM INTERCEPT MODEL") 
+print("===========================================")
+az.summary(idata_intercept)
+
+print("===========================================")
+print("           RANDOM SLOPE MODEL") 
+print("===========================================")
+az.summary(idata_slope)
+
+# az.compare will not work 
+# How can we manually compare these models? 
