49b7dd41d67914ed781cdbac45628bec04a858dd
BenjoLiving <167148510+BenjoLiving@users.noreply.github.com>
2026-01-07 15:33:39 -0700

    wsl trial

--- matching_pennies/analysis/experiments/lesion_study_paper_plots.py
+++ matching_pennies/analysis/experiments/lesion_study_paper_plots.py
@@ -21,13 +21,17 @@ PARADIGM = "normal_mp"
 
 system_os = platform.system().lower()
 
-if system_os == "windows": 
-    data_path = "C:/Users/benli/Documents/code/matching-pennies/data"
-    cores=4
-    export_path = "C:/Users/benli/Documents/projects/lesion_study/results/paper_analysis/notebook_figures/normal_mp"
-elif system_os == "darwin":
-    data_path = "/Users/ben/Documents/source/matching-pennies/data"
-    cores=1
+# if system_os == "windows": 
+#     data_path = "C:/Users/benli/Documents/code/matching-pennies/data"
+#     cores=4
+#     export_path = "C:/Users/benli/Documents/projects/lesion_study/results/paper_analysis/notebook_figures/normal_mp"
+# elif system_os == "darwin":
+#     data_path = "/Users/ben/Documents/source/matching-pennies/data"
+#     cores=1
+# elif system_os == "linux": 
+#     data_path = "/home/benjo/projects/matching-pennies/data"
+
+data_path = "/home/benjo/projects/matching-pennies/data/"
 
 tdf, sdf, manifest = load_metrics(EXPERIMENT, PARADIGM, root=data_path)
 
@@ -37,7 +41,9 @@ tdf, sdf, manifest = load_metrics(EXPERIMENT, PARADIGM, root=data_path)
 ###################################
 
 """
-The point of this figure is to show that lesions did not impact the animals ability to perform the task. 
+The point of this figure is to show that lesions did not impact the animals ability to perform 
+the task. 
+
 We look at 4 means of behavioural performance: 
     - Number of trials -> Were animals doing similar numbers of trials? 
     - Number of rewarded trials / wins -> Were the animals getting similar numbers of rewards? 
@@ -46,8 +52,23 @@ We look at 4 means of behavioural performance:
 """
 
 # Number of trials
+trial_df = (
+        sdf
+        .group_by(["treatment", "animal_id"])
+        .agg(pl.mean("NumTrials"))
+        )
 
+trial_df = trial_df.with_columns(
+    pl.col("treatment").cast(pl.Utf8)
+)
 
+trial_df = trial_df.to_pandas()
+
+sns.barplot(
+    data=trial_df,
+    x="treatment",
+    y="NumTrials"
+)
 # Number of wins 
 
 # Mean reaction time 
